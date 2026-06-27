a8b14ff3cd50dfd4687de14fd035193f350aabd1
asifali411 <asifalipm411@gmail.com>
2026-06-27 19:40:30 +0530

    feat: add to_string function for type conversion and enhance output formatting

--- src/native/io.rs
+++ src/native/io.rs
@@ -1,27 +1,12 @@
 use std::io::{self, Write};
 
-use crate::{errors::interpreter_error::InterpreterError, interpreter::{Interpreter, value::Value}};
+use crate::{errors::interpreter_error::InterpreterError, interpreter::{Interpreter, value::Value}, native::types::convert_to_string};
 
 type IResult<T> = Result<T, InterpreterError>;
 
 pub fn output(_interp: &mut Interpreter, args: Vec<Value>) -> IResult<Value> {
   for val in args {
-    match val {
-      Value::NUM(n) => print!("{}", n),
-
-      Value::NULL => print!("null"),
-
-      Value::TRUE => print!("true"),
-      Value::FALSE => print!("false"),
-      
-      Value::STRING(v) => print!("{}", v),
-
-      Value::OBJECT(obj) => print!("<instance of {}>", obj.borrow().class_name()),
-      Value::FUNC(func) => print!("<function {}>", func.func_name()),
-      Value::CLASS(cls) => print!("<class {}>", cls.name()),
-
-      Value::NativeFunction { name, ..} => print!("<native function {}>", name),
-    }
+    print!("{}", convert_to_string(&val));
     print!(" ");
   }
 
--- src/native/types.rs
+++ src/native/types.rs
@@ -22,3 +22,30 @@ pub fn to_num(_interp: &mut Interpreter, args: Vec<Value>) -> IResult<Value> {
     }
   }
 }
+
+pub fn convert_to_string(val: &Value) -> String {
+  match val {
+    Value::NUM(n) => format!("{}", n),
+    Value::STRING(n) => format!("{}", n),
+
+    Value::NULL => String::from("null"),
+    Value::TRUE => String::from("true"),
+    Value::FALSE => String::from("false"),
+
+    Value::OBJECT(obj) => format!("<instance of {}>", obj.borrow().class_name()),
+    Value::FUNC(func) => format!("<function {}>", func.func_name()),
+    Value::CLASS(cls) => format!("<class {}>", cls.name()),
+
+    Value::NativeFunction { name, ..} => format!("<native function {}>", name),
+  }
+}
+
+pub fn to_string(_interp: &mut Interpreter, args: Vec<Value>) -> IResult<Value> {
+  if args.len() > 1 {
+    return Err(InterpreterError::ArityMismatch { expected: 1, got: args.len() });
+  }
+
+  let val = convert_to_string(&args[0]);
+
+  Ok(Value::STRING(val))
+}
